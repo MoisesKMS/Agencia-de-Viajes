589cf734bb8a0430960aeaf16d75355427f9710a
Moises Jimenez Macias <MoisesJimenez@Yandex.com>
2021-02-14 02:20:25 -0600

    Inicio
    +Agregada seccion de viajes a la pagina de Inicio
    +Agregado Include de bloque_viajes.pug
    +Contenido de Viajes reutilizado en la pagina de inicio y la pagina de viajes

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,10 +1,20 @@
 import { Viaje } from '../models/Viaje.js';
 import { Testimonial } from '../models/Testimoniales.js';
 
-const paginaInicio = (req, res)=>{ //req: lo que enviamos, res: lo que express responde
-    res.render('inicio', {
-        pagina: 'Inicio'
-    });
+const paginaInicio = async (req, res)=>{ //req: lo que enviamos, res: lo que express responde
+    
+    //Consultar 3 viajes del modelo Viaje
+    try {
+        const viajes = await Viaje.findAll({limit: 3});
+
+        res.render('inicio', {
+            pagina: 'Inicio',
+            clase: 'home',
+            viajes
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const paginaNosotros = (req, res)=>{
