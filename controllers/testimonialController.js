edc877e075dc8d8f296ec5bacb34b30694d27e5a
Moises Jimenez Macias <MoisesJimenez@Yandex.com>
2021-02-14 02:03:40 -0600

    Testimoniales
    +Terminada la pagina de testimoniales
    +Agregada la consulta para mostrar testimoniales
    +Agregado el .pug para mostrar testimoniales
    +Pagina de testimoniales terminada

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,4 +1,6 @@
-const guardarTestimonial = (req, res) => {
+import { Testimonial } from '../models/Testimoniales.js'
+
+const guardarTestimonial = async (req, res) => {
     //validar
     const { nombre, correo, mensaje } = req.body;
     const errores = [];
@@ -14,16 +16,29 @@ const guardarTestimonial = (req, res) => {
     }
 
     if(errores.length > 0){
+        //Consultar testimoniales existentes
+        const testimoniales = await Testimonial.findAll();
         //Mostrar la vista con errores
         res.render('testimoniales', {
              pagina: 'Testimoniales',
              errores,
              nombre,
              correo,
-             mensaje
+             mensaje,
+             testimoniales
         });
     }else{
         //Almacenarlo en la base de datos
+        try {
+            await Testimonial.create({
+                nombre,
+                correo,
+                mensaje
+            });
+            res.redirect('testimoniales');
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
